10b8a146bdda6bf17b7c2726a00e27723a2a977d
SvetlanaOgarkova <s-eremeeva@mail.ru>
2022-09-19 18:53:12 +0300

    Code programs

---
+++ TaskCode/Program.cs
@@ -0,0 +1,20 @@
+﻿// Программа, которая из имеющегося массива строк формирует массив из строк, 
+//длина которых меньше или равна 3 символам 
+
+string[] array;
+Console.WriteLine("Введите массив строк через пробел:");
+string? stroka = Console.ReadLine();
+array = stroka!.Split(' ');
+
+void ArrayLines(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            Console.Write($"{array[i]} ");
+        }
+    }
+}
+
+ArrayLines(array);
